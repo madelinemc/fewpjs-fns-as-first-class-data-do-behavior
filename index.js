d8bc3976535f9ca961e511a98b8d57f655428336
madelinemc <madeline.coons@gmail.com>
2021-01-10 18:00:44 -0600

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
 /* Write your implementation of greet() */
+// the greet function should take on argument a string which specifies the 24h time in HH:MM
+// return greeting depending on time of day
+
+function greet(timeString) {
+  const hour = parseInt(timeString);
+  if (hour < 12) return "Good Morning";
+  if (hour > 17) return "Good Evening";
+  return "Good Afternoon";
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
